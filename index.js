a46966a42b62984b791b5bad5b557610cfbafd2e
sakibzaman1 <sakibzaman20@gmail.com>
2024-03-29 23:43:09 +0600

    Cart delete function added

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
         res.send(result);
       });
 
+      app.get('/carts/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await cartCollection.findOne(query)
+        res.send(result);
+    });
+
 
       // POST DATA
 
@@ -111,6 +118,14 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
   
 
